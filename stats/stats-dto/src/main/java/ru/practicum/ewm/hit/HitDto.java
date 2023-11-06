06190dd7ed3e9fd86127d552517e5cab18a59bca
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-11-06 22:55:52 +0300

    feat: stats-dto

---
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/hit/HitDto.java
@@ -0,0 +1,29 @@
+package ru.practicum.ewm.hit;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class HitDto {
+    @NotBlank
+    String app;
+    @NotBlank
+    String uri;
+    @NotBlank
+    String ip;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp;
+}
