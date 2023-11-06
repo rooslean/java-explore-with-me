1272a84e456906c50c564d45849500414058fb24
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-11-06 22:59:33 +0300

    refactor: stats-server

--- stats/stats-server/src/main/java/ru/practicum/ewm/stat/StatMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stat/StatMapper.java
@@ -4,22 +4,11 @@ import ru.practicum.ewm.hit.HitDto;
 
 public class StatMapper {
     public static Stat mapToStat(HitDto hitDto) {
-        Stat stat = Stat.builder()
+        return Stat.builder()
                 .app(hitDto.getApp())
                 .ip(hitDto.getIp())
                 .uri(hitDto.getUri())
                 .timestamp(hitDto.getTimestamp())
                 .build();
-        return stat;
-    }
-
-    public static HitDto mapToHitDto(Stat stat) {
-        HitDto hit = HitDto.builder()
-                .app(stat.getApp())
-                .ip(stat.getIp())
-                .uri(stat.getUri())
-                .timestamp(stat.getTimestamp())
-                .build();
-        return hit;
     }
 }
